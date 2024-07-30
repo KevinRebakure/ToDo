ea3531258970ec0479034f08e6a787b4ad4cbb9c
devark28 <82113556+devark28@users.noreply.github.com>
2024-07-30 21:36:19 +0200

    UPD: add drag capabilities, fix ToDo.jsx styling

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ACTIONS } from "../App";
+import { useDrag } from "react-dnd";
 
 export default function ToDo({ todo, dispatch, category }) {
   const [update, setUpdate] = useState(todo.task);
@@ -18,9 +19,21 @@ export default function ToDo({ todo, dispatch, category }) {
     });
   }
 
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: "card",
+    item: {
+      id: todo.id,
+      category,
+    },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }))
+
   return (
     <div
-      className={`flex items-center justify-start gap-x-3 border-l-2 ${category ? "border-green-500 bg-green-50" : "border-orange-500 bg-orange-50"} py-1 pl-3 pr-1`}
+      ref={dragRef}
+      className={`flex items-center justify-start gap-x-3 border-l-2 ${category == "completed" ? "border-green-500 bg-green-50" : "border-orange-500 bg-orange-50"} py-1 pl-3 pr-1`}
     >
       {todo.edit ? (
         <input
@@ -43,24 +56,27 @@ export default function ToDo({ todo, dispatch, category }) {
           {todo.task}
         </button>
       )}
+      {category == "completed" && (
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.COMPLETE_TODO, payload: { id: todo.id } })
+          }
+          className="ml-auto"
+        >
+          {todo.done ? (
+            <img src="./checked (1) 1.svg" alt="" className="size-[20px]" />
+          ) : (
+            <img src="./circle.png" alt="" className="size-[20px]" />
+          )}
+        </button>
+      )}
       <button
-        onClick={() =>
-          dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
-        }
-        className="ml-auto"
+        // onClick={() =>
+        //   dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
+        // }
+        className={category == "todo" ? "ml-auto" : ""}
       >
         <img src="./delete.png" alt="" className="size-[20px]" />
-      </button>
-      <button
-        onClick={() =>
-          dispatch({ type: ACTIONS.COMPLETE_TODO, payload: { id: todo.id } })
-        }
-      >
-        {todo.done ? (
-          <img src="./checked (1) 1.svg" alt="" className="size-[20px]" />
-        ) : (
-          <img src="./circle.png" alt="" className="size-[20px]" />
-        )}
       </button>
     </div>
   );
