2ec835e97bde4bfb799301e4b47b3c848e180888
devark28 <82113556+devark28@users.noreply.github.com>
2024-07-30 21:38:16 +0200

    UPD: fix add todo by button, change input styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { ACTIONS } from "../App";
 
 export default function Form({ dispatch, input, setInput }) {
@@ -7,6 +8,10 @@ export default function Form({ dispatch, input, setInput }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    insertTodo();
+  }
+
+  const insertTodo = () => {
     if (input.trim() !== "") {
       dispatch({ type: ACTIONS.ADD_TODO, payload: { task: input } });
       setInput("");
@@ -20,15 +25,15 @@ export default function Form({ dispatch, input, setInput }) {
       className="space-y-5 bg-[#FFE9D0] py-3"
     >
       <h1 className="text-center text-4xl font-semibold text-gray-700">ToDo</h1>
-      <div className="mx-auto flex w-[70%] items-center rounded-3xl border-2 border-gray-400 bg-white px-5 py-2">
+      <div className="mx-auto flex w-[60%] items-center rounded-3xl border-2 border-gray-400 bg-white overflow-hidden gap-5">
         <input
           onChange={handleChange}
           type="text"
           value={input}
           placeholder="Hit ENTER to add a new task"
-          className="w-full outline-none"
+          className="w-full outline-none px-5 py-2 bg-inherit"
         />
-        <img src="./add.png" alt="" className="size-[15px]" />
+        <img src="./add.png" alt="" className="size-[15px] mr-5" onClick={() => { insertTodo() }} />
       </div>
     </form>
   );
