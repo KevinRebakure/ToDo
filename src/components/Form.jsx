81ef63cfa7feb08103c9cbd824daf282a4f78389
Rebakure <kevinrebakure@gmail.com>
2024-08-14 16:30:43 +0200

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useDispatch, useSelector } from "react-redux";
 import { ACTIONS } from "../App";
 import { input } from "../features/formSlice";
 import { add } from "../features/todoSlice";
